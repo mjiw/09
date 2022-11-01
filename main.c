316fdd38056cea5694110284030efd014f300a84
mjiw <113112060+mjiw@users.noreply.github.com>
2022-11-01 17:23:44 +0900

    test 5

--- main.c
+++ main.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square_array(int a[],int size);
-void print_array(int a[],int size);
- 
+void addmatrix(int a[][COLS],int b[][COLS],int c[][COLS]);
+void printmatrix(int a[][COLS]);
+
 int main(int argc, char *argv[]) {
-	int list[SIZE]={1,2,3,4};
+	int a[ROWS][COLS]={
+		{2,3,0},
+		{8,9,1},
+		{7,0,5}};
 	
-	print_array(list,SIZE);
-	square_array(list,SIZE);
-	print_array(list,SIZE);
+	int b[ROWS][COLS]={
+		{1,0,0},
+		{0,1,0},
+		{0,0,1}};
+	
+	int c[ROWS][COLS];
+	
+	addmatrix(a,b,c);
+	printmatrix(c);
 	
 	return 0;
 }
 
-void square_array(int a[], int size){
+void addmatrix(int a[][COLS],int b[][COLS],int c[][COLS]){
 	int i;
-	
-	for(i=0;i<size;i++){
-	a[i]=a[i]*a[i];
+	int j;
+	for(i=0;i<ROWS;i++){
+		for(j=0;j<COLS;j++){
+			c[i][j]=a[i][j]+b[i][j];
+		}	
 	}
 }
 
-void print_array(int a[],int size){
+void printmatrix(int a[][COLS]){
 	int i;
-	
-	for(i=0;i<size;i++){
-		printf("%3d",a[i]);
+	int j;
+	for(i=0;i<ROWS;i++){
+		for(j=0;j<COLS;j++){
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");	
 	}
-	printf("\n");
+	
 }
