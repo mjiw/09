6234cb018c1ca5c64e491c96041320c40c4d983a
mjiw <113112060+mjiw@users.noreply.github.com>
2022-11-01 16:30:41 +0900

    test2-1

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SIZE 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int grade[5];
-	
-	grade[0]=10;
-	grade[1]=10;
-	grade[2]=10;
-	grade[3]=10;
-	grade[4]=10;
+	int grade[SIZE];
+	printf("5명의 점수를 입력하세요\n");
 	
 	int i;
+	for(i=0;i<SIZE;i++){
+		scanf("%d",&grade[i]);
+	}
+	
 	for(i=0;i<5;i++){
 		printf("grade[%d]=%d\n",i,grade[i]);
 	}
+	
 	return 0;
 }
